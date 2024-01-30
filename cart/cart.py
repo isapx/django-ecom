f821e7222d52786065c17ef5f9548a123749feb6
Isaias Peña <pena.649@nlsa.teleperformance.com>
2024-01-30 14:44:30 -0700

    Add Product To Cart

--- cart/cart.py
+++ cart/cart.py
@@ -11,3 +11,15 @@ class Cart():
 
         #nos aseguramos de que el carro este disponible en todas las paginas
         self.cart = cart
+
+    
+    def add(self,product):
+        product_id = str(product.id)
+
+        #logica
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
     return render(request, 'cart_summary.html',{})
 
 
 def cart_add(request):
-    pass
+    #obtenemos el carro
+    cart = Cart(request)
+    
+    #probamos el POST
+    if request.POST.get('action') == 'post':
+        #obtenemos la info
+        #product_id = int(request.POST.get('product_id'))
+        product_id = int(request.POST.get('product_id'))
+
+        #buscamos el producto en la base de datos
+       #product = get_object_or_404(Product, id=product_id)
+        product = get_object_or_404(Product, id=product_id)
+
+        #lo guadramos en la sesion
+        cart.add(product=product)
+
+        #regresamos un response
+        #response = JsonResponse({'Product Name': product.name })
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
+
 
 
 def cart_delete(request):
