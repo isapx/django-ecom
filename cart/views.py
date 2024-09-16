f1bf01c1baad61011c1192f40f1fea267fb9278a
Isaias Peña <Isaias.PenaV@teleperformance.com>
2024-09-15 17:58:39 -0700

    21. Messages

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 def cart_summary(request):
     #obtenemos el carrito
@@ -37,6 +38,7 @@ def cart_add(request):
         #response = JsonResponse({'Product Name': product.name })
         #response = JsonResponse({'Product Name: ': product.name})
         response = JsonResponse({'qty: ': cart_quantity})
+        messages.success(request, ("Producto agregado al carrito"))
         return response
 
 
@@ -47,6 +49,7 @@ def cart_delete(request):
         product_id = int(request.POST.get('product_id'))
         cart.delete(product=product_id)
     response = JsonResponse({'product':product_id})
+    messages.success(request, ("Producto eliminado del carrito"))
     return response
 
 def cart_update(request):
@@ -58,5 +61,6 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
 
         response = JsonResponse({'qty':product_qty})
+        messages.success(request, ("El carrito ha sido actualizado"))
         return response
 
